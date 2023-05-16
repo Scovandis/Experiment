6afe6b18ce48f1922808603e1aafb19f9ccdb4b8
Scovandis <andriyan354354@gmail.com>
2023-05-16 10:30:58 +0700

    auto animate

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.AnimationUtils
@@ -10,116 +12,55 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var isUp = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding.parentLayout.setOnClickListener {
-//            binding.tvTextAnimate.animate()
-//                .translationYBy(-150F)
-//                .setInterpolator(AccelerateDecelerateInterpolator())
-//                .setStartDelay(3000L)
-//                .setDuration(100L)
-//                .withEndAction {
-//                    binding.tvTextAnimate.isVisible = false
-//                }
-//                .start()
-//        }
+        binding.parentLayout.setOnClickListener {
+            isUp = !isUp
+            binding.tvTextAnimateDown.isVisible = true
+            if (isUp)
+                animateUp()
+            else
+                animateDown()
+        }
 
-//        binding.parentLayout.setOnClickListener {
-//            animateUp()
-////            animateDown()
-//        }
-        animateUp()
-        animateDown()
-
-//        val animateUp = AnimationUtils.loadAnimation(this, R.anim.slide_up)
-//        val animateDown = AnimationUtils.loadAnimation(this, R.anim.slide_down)
-//
-//        binding.parentLayout.setOnClickListener {
-//            binding.tvTextAnimate.startAnimation(animateUp)
-//        }
     }
 
     fun animateUp() {
         binding.tvTextAnimate.animate()
-            .translationYBy(-50F)
-            .setInterpolator(AccelerateDecelerateInterpolator())
-            .setStartDelay(3000L)
-            .setDuration(300L)
-            .withStartAction {
-//                binding.tvTextAnimateDown.isVisible = true
-            }
+            .alpha(0.2f)
+            .translationY((-500).dp())
+            .start()
+        binding.tvTextAnimateDown.y = 500.dp()
+        binding.tvTextAnimateDown.animate()
+            .alpha(1f)
+            .translationY(0F)
             .withEndAction {
-//                binding.tvTextAnimate.isVisible = false
-                animateDown()
+                Handler(Looper.getMainLooper()).postDelayed({animateDown()},3000)
             }
             .start()
-
-//        val animateUp = AnimationUtils.loadAnimation(this, R.anim.slide_up)
-//
-////        binding.tvTextAnimate.startAnimation(animateUp)
-//        binding.tvTextAnimateDown.startAnimation(animateUp)
-
-//        binding.tvTextAnimateDown.animate()
-//            .translationYBy(-50F)
-//            .setInterpolator(AccelerateDecelerateInterpolator())
-//            .setStartDelay(3000L)
-//            .setDuration(500L)
-//            .withEndAction {
-//            }
-//            .start()
-//        binding.tvTextAnimateDown.animate()
-//            .translationYBy(50F)
-//            .setInterpolator(AccelerateDecelerateInterpolator())
-//            .setStartDelay(3000L)
-//            .setDuration(500L)
-//            .withEndAction { binding.tvTextAnimateDown.isVisible = true }
-//            .start()
     }
 
     fun animateDown() {
-//        binding.tvTextAnimate.isVisible = true
         binding.tvTextAnimate.animate()
-            .translationYBy(50F)
-            .setInterpolator(AccelerateDecelerateInterpolator())
-            .setStartDelay(3000L)
-            .setDuration(300L)
-            .withStartAction {
-//                binding.tvTextAnimate.isVisible = true
-            }
+            .alpha(1F)
+            .translationY(0.dp())
+            .start()
+        binding.tvTextAnimateDown.y = 0.dp()
+        binding.tvTextAnimateDown.animate()
+            .alpha(0.2F)
+            .translationY(-500F)
             .withEndAction {
-//                binding.tvTextAnimateDown.isVisible = false
-//                binding.tvTextAnimate.isVisible = true
-                animateUp()
+            Handler(Looper.getMainLooper()).postDelayed({animateUp()},3000)
             }
             .start()
+    }
 
-//        val animateUp = AnimationUtils.loadAnimation(this, R.anim.slide_down)
-//
-//        binding.tvTextAnimateDown.startAnimation(animateUp)
-//        binding.tvTextAnimate.animate()
-//            .translationYBy(50F)
-//            .setInterpolator(AccelerateDecelerateInterpolator())
-//            .setStartDelay(3000L)
-//            .withEndAction {
-//                binding.tvTextAnimate.isVisible = false
-//                animateUp()
-//            }
-//            .setDuration(500L)
-//            .start()
-
-//        binding.tvTextAnimateDown.animate()
-//            .translationY(50f)
-//            .setInterpolator(AccelerateDecelerateInterpolator())
-//            .setStartDelay(3000L)
-//            .setDuration(500L)
-//            .withStartAction {binding.tvTextAnimate.isVisible = true }
-//            .withEndAction {
-//                binding.tvTextAnimateDown.isVisible = false
-//                animateUp()
-//            }
-//            .start()
+    fun Int.dp(): Float {
+        val destiny = resources.displayMetrics.density
+        return this / destiny
     }
 }
