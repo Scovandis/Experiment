dfb5d4dfa959bfba0080c0793a10970de235f411
Scovandis <andriyan354354@gmail.com>
2023-05-16 14:20:21 +0700

    material dialog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            openDialog()
+        }
+
+    }
+
+    private fun openDialog() {
+        val dialog = MaterialDialog.Builder(this).customView(R.layout.custom_dialog, false).build()
+        dialog.window!!.attributes.windowAnimations = R.style.CustomDialogAnimation
+        dialog.show()
     }
 }
